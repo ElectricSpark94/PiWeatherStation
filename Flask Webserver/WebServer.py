ca37d11f9326b18229acfa775def887ba4314d6f
ElectricSpark94 <51213134+ElectricSpark94@users.noreply.github.com>
2021-03-13 15:17:08 +0100

    Init Webserver

---
+++ Flask Webserver/WebServer.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import json
+from flask import Flask, redirect, url_for, render_template , request
+import logging
+
+
+app= Flask(__name__,static_folder='/')
+
+#with open(".Flask/templates/settings.json") as f:
+#  data = json.load(f)
+
+
+@app.route("/",methods=["POST","GET"])
+def default():
+    if request.method== "POST": 
+        #data["Settings"]["City"]= request.form["city"]    
+        #data["Settings"]["API"]= request.form["api"]
+        #data["Settings"]["Latitude"]= request.form["Latitude"]
+        #data["Settings"]["Longitude"]= request.form["Longitude"]
+        #with open("./templates/settings.json","w") as fout:
+        #    fout.write(json.dumps(data,indent=4))
+        return render_template("MainPage.html")
+    else:
+        return render_template("MainPage.html")
+        #return render_template("MainPage.html",API =data["Settings"]["API"],Latitude=data["Settings"]["Latitude"],Longitude=data["Settings"]["Longitude"],Unit=data["Settings"]["Unit"],City=data["Settings"]["City"] )
+
+@app.route("/<default>")
+def user(default):
+    return render_template("Defaultpage.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug="True" )
